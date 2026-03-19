87fa21626240b47aa7b6a637e3415ea2ac897fe5
Mateo <20253ds054@utez.edu.mx>
2026-03-19 14:02:02 -0600

    miAppFinal Clase 190326

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module mx.edu.utez.pres.miappfx {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens mx.edu.utez.pres.miappfx to javafx.fxml;
--- src/main/java/mx/edu/utez/pres/miappfx/HelloApplication.java
+++ src/main/java/mx/edu/utez/pres/miappfx/HelloApplication.java
@@ -3,17 +3,42 @@ package mx.edu.utez.pres.miappfx;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.awt.*;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        /*
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+         */
+
+        // Crear panel raíz
+        StackPane root = new StackPane();
+
+        // Crear la escena(ancho, alto)
+        Scene scene = new Scene(root, 400, 300);
+        // Configurar el stage
+        stage.setTitle("Mi app JavaFX");
+        stage.setScene(scene);
+        stage.show(); // <- mostrar la ventana
+
+        // Crear una etiqueta
+        Label etiqueta = new Label("¡Hola JavaFX!");
+
+        // Crear botón
+        Button boton = new Button("Haz click aquí");
+
+        // Definir acción del botón
+
     }
+
 }
